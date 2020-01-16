6211dfa074b40ac73716e2fc6c3d590508c7f96b
Joakim Bygdell <joakim.bygdell@nbis.se>
2020-01-16 14:54:06 +0100

    Do correct struct comparison

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"net/http/httputil"
 	"os"
 	"path"
+	"reflect"
 	"regexp"
 	"strconv"
 	"strings"
@@ -156,7 +157,7 @@ func initialization() {
 		}
 	}
 
-	if SystemCAs == nil {
+	if reflect.DeepEqual(SystemCAs, x509.NewCertPool()) {
 		fmt.Println("creating new CApool")
 		SystemCAs = x509.NewCertPool()
 	}
