ade427a1d359bc3a47aa18e91b7f86e47487d6ce
Joakim Bygdell <joakim.bygdell@nbis.se>
2020-01-16 15:22:31 +0100

    Test main.buildMQURI

--- main_test.go
+++ main_test.go
@@ -44,3 +44,10 @@ func TestInitialization_NoCaCerts(t *testing.T) {
 	SystemCAs = x509.NewCertPool()
 	initialization()
 }
+
+func TestBuildMqURI(t *testing.T) {
+	amqps := buildMqURI("localhost", "5555", "mquser", "mqpass", "/vhost", true)
+	assert.Equal(t, "amqps://mquser:mqpass@localhost:5555/vhost", amqps)
+	amqp := buildMqURI("localhost", "5555", "mquser", "mqpass", "/vhost", false)
+	assert.Equal(t, "amqp://mquser:mqpass@localhost:5555/vhost", amqp)
+}
