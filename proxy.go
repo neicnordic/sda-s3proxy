87661d62ed9f0fe5e25cb0ae4d8c3d314964eb16
Johan Viklund <johan.viklund@nbis.se>
2020-02-14 14:29:26 +0100

    Create proxy module and tests for it.
    To get tests pass at this stage I had to convert main.go to use the new
    proxy.

---
+++ proxy.go
@@ -0,0 +1,233 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/minio/minio-go/v6/pkg/s3signer"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// Proxy represents the toplevel object in this application
+type Proxy struct {
+	s3        S3Config
+	auth      Authenticator
+	messenger Messenger
+	tlsConfig *tls.Config
+}
+
+// S3RequestType is the type of request that we are currently proxying to the
+// backend
+type S3RequestType int
+
+// The different types of requests
+const (
+	MakeBucket S3RequestType = iota
+	RemoveBucket
+	List
+	Put
+	Get
+	Delete
+	AbortMultipart
+	Policy
+	Other
+)
+
+// NewProxy creates a new S3Proxy. This implements the ServerHTTP interface.
+func NewProxy(s3conf S3Config, auth Authenticator, messenger Messenger, tls *tls.Config) *Proxy {
+	log.SetLevel(log.InfoLevel)
+	return &Proxy{s3conf, auth, messenger, tls}
+}
+
+func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch t := p.detectRequestType(r); t {
+	case MakeBucket, RemoveBucket, Delete, Policy, Get:
+		// Not allowed
+		log.Debug("Not allowed 1")
+		p.notAllowedResponse(w, r)
+	case Put, List, Other, AbortMultipart:
+		// Allowed
+		p.allowedResponse(w, r)
+	default:
+		log.Debug("Not allowed 2")
+		p.notAllowedResponse(w, r)
+	}
+}
+
+func (p *Proxy) internalServerError(w http.ResponseWriter, r *http.Request) {
+	log.Debug("Internal Server Error")
+	w.WriteHeader(500)
+}
+
+func (p *Proxy) notAllowedResponse(w http.ResponseWriter, r *http.Request) {
+	log.Debug("Not Allowed Response")
+	w.WriteHeader(403)
+}
+
+func (p *Proxy) notAuthorized(w http.ResponseWriter, r *http.Request) {
+	log.Debug("Not Authorized")
+	w.WriteHeader(401) // Actually correct!
+}
+
+func (p *Proxy) allowedResponse(w http.ResponseWriter, r *http.Request) {
+	if err := p.auth.Authenticate(r); err != nil {
+		fmt.Println("Not authenticated !!!!!! !!  !!!")
+		fmt.Println(err)
+		p.notAuthorized(w, r)
+		return
+	}
+
+	log.Debug("Prepend")
+	p.prependBucketToHostPath(r)
+
+	log.Debug("Forward to Backend")
+	s3response, err := p.forwardToBackend(r)
+
+	if err != nil {
+		log.Debug("Internal server error")
+		fmt.Println(err)
+		p.internalServerError(w, r)
+		return
+	}
+
+	// Send message to upstream
+	if p.uploadFinishedSuccessfully(r, s3response) {
+		message, _ := CreateMessageFromRequest(r)
+		if err = p.messenger.SendMessage(message); err != nil {
+			log.Printf("error when sending message: %v", err)
+		}
+	}
+
+	// Redirect answer
+	for header, values := range s3response.Header {
+		for _, value := range values {
+			w.Header().Add(header, value)
+		}
+	}
+	_, err = io.Copy(w, s3response.Body)
+	if err != nil {
+		log.Fatalln("redirect error")
+	}
+}
+
+func (p *Proxy) uploadFinishedSuccessfully(req *http.Request, response *http.Response) bool {
+	if response.StatusCode != 200 {
+		return false
+	} else if req.Method == http.MethodPut && !strings.Contains(req.URL.String(), "partNumber") {
+		return true
+	} else if req.Method == http.MethodPost && strings.Contains(req.URL.String(), "uploadId") {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (p *Proxy) forwardToBackend(r *http.Request) (*http.Response, error) {
+	tr := &http.Transport{TLSClientConfig: p.tlsConfig}
+	client := &http.Client{Transport: tr}
+
+	p.resignHeader(r, p.s3.accessKey, p.s3.secretKey, p.s3.url)
+
+	// Redirect request
+	nr, err := http.NewRequest(r.Method, p.s3.url+r.URL.String(), r.Body)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	nr.Header = r.Header
+	contentLength, _ := strconv.ParseInt(r.Header.Get("content-length"), 10, 64)
+	nr.ContentLength = contentLength
+	return client.Do(nr)
+}
+
+// Add bucket to host path
+func (p *Proxy) prependBucketToHostPath(r *http.Request) {
+	bucket := p.s3.bucket
+
+	// Extract username for request's url path
+	re := regexp.MustCompile("/([^/]+)/")
+	username := re.FindStringSubmatch(r.URL.Path)[1]
+
+	log.Debug("Incomming Path: ", r.URL.Path)
+	log.Debug("Incomming Raw: ", r.URL.RawQuery)
+
+	// Restructure request to query the users folder instead of the general bucket
+	if r.Method == http.MethodGet && strings.Contains(r.URL.String(), "?delimiter") {
+		r.URL.Path = "/" + bucket + "/"
+		if strings.Contains(r.URL.RawQuery, "&prefix") {
+			params := strings.Split(r.URL.RawQuery, "&prefix=")
+			r.URL.RawQuery = params[0] + "&prefix=" + username + "%2F" + params[1]
+		} else {
+			r.URL.RawQuery = r.URL.RawQuery + "&prefix=" + username + "%2F"
+		}
+		log.Debug("New RawQuery: ", r.URL.RawQuery)
+	} else if r.Method == http.MethodGet && strings.Contains(r.URL.String(), "?location") {
+		r.URL.Path = "/" + bucket + "/"
+		log.Debug("New Path: ", r.URL.Path)
+	} else if r.Method == http.MethodPost || r.Method == http.MethodPut {
+		r.URL.Path = "/" + bucket + r.URL.Path
+		log.Debug("New Path: ", r.URL.Path)
+	}
+}
+
+// Function for signing the headers of the s3 requests
+// Used for for creating a signature for with the default
+// credentials of the s3 service and the user's signature (authentication)
+func (p *Proxy) resignHeader(r *http.Request, accessKey string, secretKey string, backendURL string) *http.Request {
+	if strings.Contains(backendURL, "//") {
+		host := strings.SplitN(backendURL, "//", 2)
+		r.Host = host[1]
+	}
+	return s3signer.SignV4(*r, accessKey, secretKey, "", p.s3.region)
+}
+
+// Not necesarily a function on the struct since it does not use any of the
+// members.
+func (p *Proxy) detectRequestType(r *http.Request) S3RequestType {
+	switch r.Method {
+	case http.MethodGet:
+		if strings.HasSuffix(r.URL.String(), "/") {
+			log.Debug("detect Get")
+			return Get
+		} else if strings.Contains(r.URL.String(), "?acl") {
+			log.Debug("detect Policy")
+			return Policy
+		} else {
+			log.Debug("detect List")
+			return List
+		}
+	case http.MethodDelete:
+		if strings.HasSuffix(r.URL.String(), "/") {
+			log.Debug("detect RemoveBucket")
+			return RemoveBucket
+		} else if strings.Contains(r.URL.String(), "uploadId") {
+			log.Debug("detect AbortMultipart")
+			return AbortMultipart
+		} else {
+			// Do we allow deletion of files?
+			log.Debug("detect Delete")
+			return Delete
+		}
+	case http.MethodPut:
+		if strings.HasSuffix(r.URL.String(), "/") {
+			log.Debug("detect MakeBucket")
+			return MakeBucket
+		} else if strings.Contains(r.URL.String(), "?policy") {
+			log.Debug("detect Policy")
+			return Policy
+		} else {
+			// Should decide if we will handle copy here or through authentication
+			log.Debug("detect Put")
+			return Put
+		}
+	default:
+		log.Debug("detect Other")
+		return Other
+	}
+}
---
+++ proxy_test.go
@@ -0,0 +1,203 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type FakeServer struct {
+	ts     *httptest.Server
+	pinged bool
+}
+
+func startFakeServer() *FakeServer {
+	l, err := net.Listen("tcp", "127.0.0.1:9023")
+	if err != nil {
+		panic(fmt.Errorf("Can't create mock server for testing: %s", err))
+	}
+	f := FakeServer{}
+	foo := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { f.pinged = true })
+	ts := httptest.NewUnstartedServer(foo)
+	ts.Listener.Close()
+	ts.Listener = l
+	ts.Start()
+
+	f.ts = ts
+
+	return &f
+}
+
+func (f *FakeServer) Close() {
+	f.ts.Close()
+}
+
+func (f *FakeServer) PingedAndRestore() bool {
+	ret := f.pinged
+	f.pinged = false
+	return ret
+}
+
+type MockMessenger struct {
+	lastEvent *Event
+}
+
+func NewMockMessenger() *MockMessenger {
+	return &MockMessenger{nil}
+}
+
+func (m *MockMessenger) SendMessage(event Event) error {
+	m.lastEvent = &event
+	return nil
+}
+
+func (m *MockMessenger) CheckAndRestore() bool {
+	if m.lastEvent == nil {
+		return false
+	}
+	m.lastEvent = nil
+	return true
+}
+
+func TestServeHTTP_disallowed(t *testing.T) {
+	// Start fake server
+	f := startFakeServer()
+	defer f.Close()
+
+	s3conf := S3Config{
+		url:       "http://localhost:9023",
+		accessKey: "someAccess",
+		secretKey: "someSecret",
+		bucket:    "buckbuck",
+		region:    "us-east-1",
+		cacert:    "./dev_utils/certs/ca.crt",
+	}
+	messenger := NewMockMessenger()
+	proxy := NewProxy(s3conf, NewAlwaysAllow(), messenger, new(tls.Config))
+
+	r, _ := http.NewRequest("", "", nil)
+	w := httptest.NewRecorder()
+
+	// Remove bucket disallowed
+	r.Method = "DELETE"
+	r.URL, _ = url.Parse("/asdf/")
+	proxy.ServeHTTP(w, r)
+	assert.Equal(t, 403, w.Result().StatusCode)
+	assert.Equal(t, false, f.PingedAndRestore())
+	assert.Equal(t, false, messenger.CheckAndRestore())
+
+	// Deletion of files are dissallowed
+	r.Method = "DELETE"
+	r.URL, _ = url.Parse("/asdf/")
+	proxy.ServeHTTP(w, r)
+	assert.Equal(t, 403, w.Result().StatusCode)
+	assert.Equal(t, false, f.PingedAndRestore())
+	assert.Equal(t, false, messenger.CheckAndRestore())
+
+	// Policy methods are not allowed
+	w = httptest.NewRecorder()
+	r.Method = "GET"
+	r.URL, _ = url.Parse("/asdf?acl=rw")
+	proxy.ServeHTTP(w, r)
+	assert.Equal(t, 403, w.Result().StatusCode)
+	assert.Equal(t, false, f.PingedAndRestore())
+	assert.Equal(t, false, messenger.CheckAndRestore())
+
+	// Normal get is dissallowed
+	w = httptest.NewRecorder()
+	r.Method = "GET"
+	r.URL, _ = url.Parse("/asdf/")
+	proxy.ServeHTTP(w, r)
+	assert.Equal(t, 403, w.Result().StatusCode)
+	assert.Equal(t, false, f.PingedAndRestore())
+	assert.Equal(t, false, messenger.CheckAndRestore())
+
+	// Put policy is disallowed
+	w = httptest.NewRecorder()
+	r.Method = "PUT"
+	r.URL, _ = url.Parse("/asdf?policy=rw")
+	proxy.ServeHTTP(w, r)
+	assert.Equal(t, 403, w.Result().StatusCode)
+	assert.Equal(t, false, f.PingedAndRestore())
+	assert.Equal(t, false, messenger.CheckAndRestore())
+}
+
+func TestServeHTTP_allowed(t *testing.T) {
+	// Start fake server
+	f := startFakeServer()
+	defer f.Close()
+
+	// Start proxy
+	s3conf := S3Config{
+		url:       "http://localhost:9023",
+		accessKey: "someAccess",
+		secretKey: "someSecret",
+		bucket:    "buckbuck",
+		region:    "us-east-1",
+		cacert:    "./dev_utils/certs/ca.crt",
+	}
+	messenger := NewMockMessenger()
+	proxy := NewProxy(s3conf, NewAlwaysAllow(), messenger, new(tls.Config))
+	//proxy := NewProxy(s3conf, NewValidateFromFile("./dev_utils/users.csv"), NewMockMessenger(), s, new(tls.Config))
+
+	// List files works
+	r, _ := http.NewRequest("GET", "/username/file", nil)
+	w := httptest.NewRecorder()
+	proxy.ServeHTTP(w, r)
+	assert.Equal(t, 200, w.Result().StatusCode)
+	assert.Equal(t, true, f.PingedAndRestore())
+	assert.Equal(t, false, f.PingedAndRestore()) // Testing the pinged interface
+	assert.Equal(t, false, messenger.CheckAndRestore())
+
+	// Put file works
+	w = httptest.NewRecorder()
+	r.Method = "PUT"
+	proxy.ServeHTTP(w, r)
+	assert.Equal(t, 200, w.Result().StatusCode)
+	assert.Equal(t, true, f.PingedAndRestore())
+	assert.Equal(t, true, messenger.CheckAndRestore())
+	assert.Equal(t, false, messenger.CheckAndRestore())
+
+	// Put with partnumber sends no message
+	w = httptest.NewRecorder()
+	r.Method = "PUT"
+	r.URL, _ = url.Parse("/username/file?partNumber=5")
+	proxy.ServeHTTP(w, r)
+	assert.Equal(t, 200, w.Result().StatusCode)
+	assert.Equal(t, true, f.PingedAndRestore())
+	assert.Equal(t, false, messenger.CheckAndRestore())
+
+	// Post with uploadId sends message
+	r.Method = "POST"
+	r.URL, _ = url.Parse("/username/file?uploadId=5")
+	w = httptest.NewRecorder()
+	proxy.ServeHTTP(w, r)
+	assert.Equal(t, 200, w.Result().StatusCode)
+	assert.Equal(t, true, f.PingedAndRestore())
+	assert.Equal(t, true, messenger.CheckAndRestore())
+
+	// Post without uploadId sends no message
+	r.Method = "POST"
+	r.URL, _ = url.Parse("/username/file")
+	w = httptest.NewRecorder()
+	proxy.ServeHTTP(w, r)
+	assert.Equal(t, 200, w.Result().StatusCode)
+	assert.Equal(t, true, f.PingedAndRestore())
+	assert.Equal(t, false, messenger.CheckAndRestore())
+
+	// Abort multipart works
+	r.Method = "POST"
+	r.Method = "DELETE"
+	r.URL, _ = url.Parse("/asdf/asdf?uploadId=123")
+	w = httptest.NewRecorder()
+	proxy.ServeHTTP(w, r)
+	assert.Equal(t, 200, w.Result().StatusCode)
+	assert.Equal(t, true, f.PingedAndRestore())
+	assert.Equal(t, false, messenger.CheckAndRestore())
+}
