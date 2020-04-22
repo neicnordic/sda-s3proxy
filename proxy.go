c3dd6f86bb2752052de7b99de2b16599b85f8061
Joakim Bygdell <joakim.bygdell@nbis.se>
2020-04-22 08:46:53 +0200

    Take bucket from config object instead of directly from viper.
    Co-Authored-By: Johan Viklund <johan.viklund@nbis.se>

--- proxy.go
+++ proxy.go
@@ -19,7 +19,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/minio/minio-go/v6/pkg/s3signer"
 	log "github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
 )
 
 // Proxy represents the toplevel object in this application
@@ -285,7 +284,7 @@ func (p *Proxy) CreateMessageFromRequest(r *http.Request) (Event, error) {
 // RequestInfo is a function that makes a request to the S3 and collects
 // the etag and size information for the uploaded document
 func (p *Proxy) requestInfo(fullPath string) (string, int64, error) {
-	filePath := strings.Replace(fullPath, "/"+viper.GetString("aws.bucket")+"/", "", 1)
+	filePath := strings.Replace(fullPath, "/"+p.s3.bucket+"/", "", 1)
 	s, err := p.newSession()
 	if err != nil {
 		return "", 0, err
