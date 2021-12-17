19c093d342064b760b49c7afd8e9186f2a4bb517
Martin Norling <norlingjr@gmail.com>
2021-12-17 15:15:55 +0100

    Add recover func to remove panic tracebacks in log

--- main.go
+++ main.go
@@ -7,6 +7,13 @@ import (
 )
 
 func main() {
+	// Create a function to handle panic and exit gracefully
+	defer func() {
+		if err := recover(); err != nil {
+			log.Fatal("Could not recover, exiting")
+		}
+	}()
+
 	config, err := NewConfig()
 	if err != nil {
 		log.Fatal(err)
@@ -34,12 +41,14 @@ func main() {
 	// Load keys for JWT verification
 	if config.Server.jwtpubkeyurl != "" {
 		if err := auth.getjwtpubkey(config.Server.jwtpubkeyurl); err != nil {
-			log.Panicf("Error while getting key %s: %v", config.Server.jwtpubkeyurl, err)
+			log.Panicf("Error while getting key %s: %v",
+				config.Server.jwtpubkeyurl, err)
 		}
 	}
 	if config.Server.jwtpubkeypath != "" {
 		if err := auth.getjwtkey(config.Server.jwtpubkeypath); err != nil {
-			log.Panicf("Error while getting key %s: %v", config.Server.jwtpubkeypath, err)
+			log.Panicf("Error while getting key %s: %v",
+				config.Server.jwtpubkeypath, err)
 		}
 	}
 	proxy := NewProxy(config.S3, auth, messenger, tlsProxy)
@@ -52,7 +61,8 @@ func main() {
 	go hc.RunHealthChecks()
 
 	if config.Server.cert != "" && config.Server.key != "" {
-		if e := http.ListenAndServeTLS(":8000", config.Server.cert, config.Server.key, nil); e != nil {
+		if e := http.ListenAndServeTLS(":8000", config.Server.cert,
+			config.Server.key, nil); e != nil {
 			panic(e)
 		}
 	} else {
