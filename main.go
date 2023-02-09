633f0a74dd4c7e8036ee1942b805166a8c390acc
Joakim Bygdell <Joakim.Bygdell@nbis.se>
2023-02-09 07:57:48 +0100

    Make Config public

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// Export Conf so we can access it in the other modules
+var Conf *Config
+
 func main() {
 	// Create a function to handle panic and exit gracefully
 	defer func() {
@@ -16,20 +19,22 @@ func main() {
 		}
 	}()
 
-	config, err := NewConfig()
+	c, err := NewConfig()
 	if err != nil {
 		log.Panic(err)
 	}
-	tlsBroker, err := TLSConfigBroker(config)
+	Conf = c
+
+	tlsBroker, err := TLSConfigBroker(Conf)
 	if err != nil {
 		log.Panic(err)
 	}
-	tlsProxy, err := TLSConfigProxy(config)
+	tlsProxy, err := TLSConfigProxy(Conf)
 	if err != nil {
 		log.Panic(err)
 	}
 
-	sdaDB, err := common.NewSDAdb(config.DB)
+	sdaDB, err := common.NewSDAdb(Conf.DB)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -38,35 +43,35 @@ func main() {
 
 	log.Debugf("Connected to sda-db (v%v)", sdaDB.Version)
 
-	err = checkS3Bucket(config.S3)
+	err = checkS3Bucket(Conf.S3)
 	if err != nil {
 		log.Panic(err)
 	}
 
-	messenger := NewAMQPMessenger(config.Broker, tlsBroker)
+	messenger := NewAMQPMessenger(Conf.Broker, tlsBroker)
 	log.Debug("messenger acquired ", messenger)
 
 	var pubkeys map[string][]byte
 	auth := NewValidateFromToken(pubkeys)
 	auth.pubkeys = make(map[string][]byte)
 	// Load keys for JWT verification
-	if config.Server.jwtpubkeyurl != "" {
-		if err := auth.getjwtpubkey(config.Server.jwtpubkeyurl); err != nil {
-			log.Panicf("Error while getting key %s: %v", config.Server.jwtpubkeyurl, err)
+	if Conf.Server.jwtpubkeyurl != "" {
+		if err := auth.getjwtpubkey(Conf.Server.jwtpubkeyurl); err != nil {
+			log.Panicf("Error while getting key %s: %v", Conf.Server.jwtpubkeyurl, err)
 		}
 	}
-	if config.Server.jwtpubkeypath != "" {
-		if err := auth.getjwtkey(config.Server.jwtpubkeypath); err != nil {
-			log.Panicf("Error while getting key %s: %v", config.Server.jwtpubkeypath, err)
+	if Conf.Server.jwtpubkeypath != "" {
+		if err := auth.getjwtkey(Conf.Server.jwtpubkeypath); err != nil {
+			log.Panicf("Error while getting key %s: %v", Conf.Server.jwtpubkeypath, err)
 		}
 	}
-	proxy := NewProxy(config.S3, auth, messenger, sdaDB, tlsProxy)
+	proxy := NewProxy(Conf.S3, auth, messenger, sdaDB, tlsProxy)
 
 	log.Debug("got the proxy ", proxy)
 
 	http.Handle("/", proxy)
 
-	hc := NewHealthCheck(8001, config.S3, config.Broker, tlsProxy)
+	hc := NewHealthCheck(8001, Conf.S3, Conf.Broker, tlsProxy)
 	go hc.RunHealthChecks()
 
 	server := &http.Server{
@@ -77,8 +82,8 @@ func main() {
 		ReadHeaderTimeout: 30 * time.Second,
 	}
 
-	if config.Server.cert != "" && config.Server.key != "" {
-		if err := server.ListenAndServeTLS(config.Server.cert, config.Server.key); err != nil {
+	if Conf.Server.cert != "" && Conf.Server.key != "" {
+		if err := server.ListenAndServeTLS(Conf.Server.cert, Conf.Server.key); err != nil {
 			panic(err)
 		}
 	} else {
